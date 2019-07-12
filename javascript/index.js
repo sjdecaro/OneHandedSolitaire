1c4ad5efaf0f48a28209ac32b20307030c71fca9
Sharon DeCaro <Sdecaro@imo-online.com>
2019-07-12 17:20:31 -0500

    docs: outline game rules for main program

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+console.log(`Welcome! Let's play One-Handed Solitaire!`);
+
+// TODO: Create a new, shuffled deck of cards.
+
+// TODO: Draw the first 5 cards as the initial hand.
+
+// TODO: Compare the last card drawn to the card 4 positions ahead of it.
+// If the same value, discard all 5 cards.
+// If the same suit, discard middle 3 cards.
+//   Once discarded, run these two comparisons again.
+// If hand size < 5, draw cards until hand size is 5.
+// If different, draw 1 card.
+
+// TODO: Repeat steps until all cards have been drawn.
+
+// TODO: Return a message letting the player know if they have won or lost.
