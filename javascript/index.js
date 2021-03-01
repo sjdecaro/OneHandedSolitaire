83a67f91eb9e2fdeab1d169186c0a3225efd7a3b
Sharon DeCaro <asliceofpi@gmail.com>
2021-03-01 01:15:15 +0000

    Check matches with each drawn card

--- javascript/index.js
+++ javascript/index.js
@@ -12,14 +12,17 @@ let hand = playDeck.splice(0, 5);
 // Draw each remaining card from the deck.
 
 while (playDeck.length > 0) {
-    readline.question(`Hit Enter to draw a card.`, foo => {
+    readline.question(`Hit Enter to draw a card. `, foo => {
         readline.close();
     });
 
-    next_card = playDeck.splice(0, 1);
-    hand = hand.concat(next_card);
+    next_card = playDeck.splice(0, 1)[0];
+    hand.push(next_card);
 
-    console.log("You drew the " + next_card[0].value + " of " + next_card[0].suit + ".");
+    console.log(next_card);
+    console.log(hand);
+
+    console.log("You drew the " + next_card.value + " of " + next_card.suit + ".");
 
     // TODO: Compare the last card drawn to the card 4 positions ahead of it.
     // If the same value, discard all 5 cards.
@@ -27,6 +30,17 @@ while (playDeck.length > 0) {
     //   Once discarded, run these two comparisons again.
     // If hand size < 5, draw cards until hand size is 5.
     // If different, draw 1 card.
+
+    comparison_card = hand[hand.length - 5];
+    console.log("Checking the " + comparison_card.value + " of " + comparison_card.suit + "...");
+
+    if (next_card.suit == comparison_card.suit) {
+        console.log("Hey, the suits match! Discard the middle 3 cards.")
+    } else if (next_card.value == comparison_card.value) {
+        console.log("WOW, the values match!! Discard all 5 cards.")
+    } else {
+        console.log("Darn, no match. Keep going!");
+    }
 
     // TODO: Repeat steps until all cards have been drawn.
 
